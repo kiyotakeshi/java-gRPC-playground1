a2d8dd4b4b10eb98d03a467c47599418ea11ed4d
Kiyota iMac <kiyotatakeshi0609@gmail.com>
2022-03-17 08:43:46 +0900

    server-side streaming

--- src/main/java/com/kiyotakeshi/server/BankService.java
+++ src/main/java/com/kiyotakeshi/server/BankService.java
@@ -1,8 +1,6 @@
 package com.kiyotakeshi.server;
 
-import com.kiyotakeshi.models.Balance;
-import com.kiyotakeshi.models.BalanceCheckRequest;
-import com.kiyotakeshi.models.BankServiceGrpc;
+import com.kiyotakeshi.models.*;
 import io.grpc.stub.StreamObserver;
 
 public class BankService extends BankServiceGrpc.BankServiceImplBase {
@@ -17,6 +15,30 @@ public class BankService extends BankServiceGrpc.BankServiceImplBase {
                 .build();
 
         responseObserver.onNext(balance);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void withdraw(WithdrawRequest request, StreamObserver<Money> responseObserver) {
+
+        int accountNumber = request.getAccountNumber();
+        int amount = request.getAmount(); // 10, 20, 30...
+
+        int balance = AccountDatabase.getBalance(accountNumber);
+
+        // リクエストを分割して処理する
+        for (int i = 0; i < (amount / 10); i++) {
+            var money = Money.newBuilder().setValue(10).build();
+            responseObserver.onNext(money);
+            AccountDatabase.deductBalance(accountNumber, 10);
+            try {
+                // 1秒停止を入れることで server-side streaming が動作しているのをわかりやすくする
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
         responseObserver.onCompleted();
     }
 }
