9c172d80369b895ff6dea9b8714c3cdd6b971a47
Kiyota iMac <kiyotatakeshi0609@gmail.com>
2022-03-17 08:51:19 +0900

    add validation and error handling

--- src/main/java/com/kiyotakeshi/server/BankService.java
+++ src/main/java/com/kiyotakeshi/server/BankService.java
@@ -1,6 +1,7 @@
 package com.kiyotakeshi.server;
 
 import com.kiyotakeshi.models.*;
+import io.grpc.Status;
 import io.grpc.stub.StreamObserver;
 
 public class BankService extends BankServiceGrpc.BankServiceImplBase {
@@ -26,17 +27,24 @@ public class BankService extends BankServiceGrpc.BankServiceImplBase {
 
         int balance = AccountDatabase.getBalance(accountNumber);
 
+        if(balance < amount) {
+            // @see https://developers.google.com/maps-booking/reference/grpc-api-v2/status_codes
+            Status status = Status.FAILED_PRECONDITION.withDescription("no enough money. you only have " + balance);
+            responseObserver.onError(status.asRuntimeException());
+            return;
+        }
+
         // リクエストを分割して処理する
         for (int i = 0; i < (amount / 10); i++) {
             var money = Money.newBuilder().setValue(10).build();
             responseObserver.onNext(money);
             AccountDatabase.deductBalance(accountNumber, 10);
-            try {
-                // 1秒停止を入れることで server-side streaming が動作しているのをわかりやすくする
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+//            try {
+//                // 1秒停止を入れることで server-side streaming が動作しているのをわかりやすくする
+//                Thread.sleep(1000);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
         }
 
         responseObserver.onCompleted();
